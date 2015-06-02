2b4fe2327f1abd1e85ab0604f2d6b43cd77570bb
Christian(Titan) <christian@neslin.se>
2015-06-02 19:57:46 +0200

    Matching should be fixed. Added code for stripping a pesky comma. Added reading of command line args, prompt for code if none.

--- PycharmProjects/plates/plates.py
+++ PycharmProjects/plates/plates.py
@@ -1,15 +1,22 @@
 __author__ = 'neslin'
 # App to find russian region from license plate number
 
-
+import sys
 import bs4
 import re
 
+if len(sys.argv) < 2:
+    search = str(input('Enter area code:'))
+else:
+    search = sys.argv[1]
+# search = '77'
+
+
 htmlFile = open('plates.html', 'rb')
 soup = bs4.BeautifulSoup(htmlFile)
-# search = str(input('Enter area code:'))
-search = '3123'
-table = soup.find('table', attrs={'class':'wikitable'})
+
+table = soup.find('table', attrs={'class': 'wikitable'})
+found = False
 
 rows = []
 rowsDict = {}
@@ -18,18 +25,22 @@ rowsDict = {}
 for row in table.findAll('tr'):
     cells = []
     for cell in row.findAll('td'):
-        text = re.sub(r'^0|(\[2\]|\(|\))','', cell.getText()) # Strip leading zeros and [2]
+        text = re.sub(r'^0|(\[2\]|\(|\)|)', '', cell.getText())  # Strip leading zeros and [2]
         cells.append(text)
         rows.append(cells)
-
 
 for row in rows:
     rowsDict[row[1]] = row[0]
 
 for region, code in rowsDict.items():
     # Try to match search string in the beginning, middle and end
-    m = re.search(r'(^{}$)|(^{}\D)|(\D{}\D)|(\D{}$)'.format(search, search, search, search), code)
-    if m:
-        print(m.group(), region)
-    else:
-        print("Not found")
+    matchGroup = re.search(r'(^{}$)|(^{}\D)|(\D{}\D)|(\D{}$)'.format(search, search, search, search), code)
+    if matchGroup:
+        match = matchGroup.group()
+        print('Area code {} belongs to {}'.format(match.strip(','), region))
+        found = True
+
+if not found:
+    print("Not found")
+
+# ^{}$)|(^{}\D)|(\D{}\D)|(\D{}$
